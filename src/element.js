94a9e525979d921b015cbe7ac019221fd7a962c3
Mladen Ilic <mladenilic480@gmail.com>
2019-05-19 19:55:48 +0200

    Initial commit

---
+++ src/element.js
@@ -0,0 +1,14 @@
+export default ({ tagName = '', content = [], attributes = {}, events = {} }) => {
+  let element = document.createElement(tagName || 'div');
+
+  Object.keys(attributes).forEach((key) => element.setAttribute(key, attributes[key]));
+  Object.keys(events).forEach((event) => element.addEventListener(event, events[event]));
+
+  if (typeof content === 'string') {
+    element.innerHTML = content;
+  } else if (Array.isArray(content) && content.length > 0) {
+    content.forEach((child) => element.append(this.html(child)));
+  }
+
+  return element;
+};
---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const path = require('path');
+
+module.exports = [{
+  entry: require.resolve('./src/element.js'),
+  context: __dirname,
+  mode: 'production',
+  output: {
+      path: path.resolve(__dirname, './'),
+      filename: 'dist/element.min.js',
+      library: 'element',
+      libraryExport: 'default',
+      libraryTarget: 'umd'
+  },
+  module: {
+      rules: [
+          {
+              test: /\.js$/,
+              loader: 'babel-loader'
+          }
+      ]
+  }
+}];
