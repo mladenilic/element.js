f7292b1d9085622fe3cbd746fb5962c94bfe9ce0
Mladen Ilic <mladenilic480@gmail.com>
2019-05-19 21:42:25 +0200

    Fix recursive call

--- src/element.js
+++ src/element.js
@@ -1,14 +1,16 @@
-export default ({ tagName = '', content = [], attributes = {}, events = {} } = {}) => {
-  let element = document.createElement(tagName || 'div');
+const element = ({ tagName = '', content = [], attributes = {}, events = {} } = {}) => {
+  let node = document.createElement(tagName || 'div');
 
-  Object.keys(attributes).forEach((key) => element.setAttribute(key, attributes[key]));
-  Object.keys(events).forEach((event) => element.addEventListener(event, events[event]));
+  Object.keys(attributes).forEach((key) => node.setAttribute(key, attributes[key]));
+  Object.keys(events).forEach((event) => node.addEventListener(event, events[event]));
 
   if (typeof content === 'string') {
-    element.innerHTML = content;
+    node.innerHTML = content;
   } else if (Array.isArray(content) && content.length > 0) {
-    content.forEach((child) => element.append(this.html(child)));
+    content.forEach((child) => node.append(element(child)));
   }
 
-  return element;
+  return node;
 };
+
+export default element;
