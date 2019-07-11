4d66c2ed3ba8814e7e5772fe4868275231f899d0
Mladen Ilic <mladenilic480@gmail.com>
2019-07-11 12:40:32 +0200

    Implement passing namespace to createElementNS

--- src/element.js
+++ src/element.js
@@ -1,7 +1,23 @@
-const element = ({ tagName = '', content = [], attributes = {}, events = {} } = {}) => {
-  let node = document.createElement(tagName || 'div');
+const createElement = (namespace, tagName) => {
+  if (namespace && namespace.length > 0) {
+    return document.createElementNS(namespace, tagName);
+  }
 
-  Object.keys(attributes).forEach((key) => node.setAttribute(key, attributes[key]));
+  return document.createElement(tagName || 'div');
+};
+
+const setAttribute = (node, namespace, name, value) => {
+  if (namespace && namespace.length > 0) {
+    return node.setAttributeNS(namespace, name, value);
+  }
+
+  return node.setAttribute(name, value);
+};
+
+const element = ({ tagName = '', content = [], attributes = {}, events = {}, namespace = '' } = {}) => {
+  let node = createElement(namespace, tagName);
+
+  Object.keys(attributes).forEach((key) => setAttribute(node, namespace, key, attributes[key]));
   Object.keys(events).forEach((event) => node.addEventListener(...[event].concat(events[event])));
 
   if (typeof content === 'string') {
