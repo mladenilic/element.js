f646330ed81dab1c84b982c26d1f4e12009e578b
Mladen Ilic <mladenilic480@gmail.com>
2019-07-11 12:43:52 +0200

    Skip using setAttributeNS

--- src/element.js
+++ src/element.js
@@ -6,18 +6,10 @@ const createElement = (namespace, tagName) => {
   return document.createElement(tagName || 'div');
 };
 
-const setAttribute = (node, namespace, name, value) => {
-  if (namespace && namespace.length > 0) {
-    return node.setAttributeNS(namespace, name, value);
-  }
-
-  return node.setAttribute(name, value);
-};
-
 const element = ({ tagName = '', content = [], attributes = {}, events = {}, namespace = '' } = {}) => {
   let node = createElement(namespace, tagName);
 
-  Object.keys(attributes).forEach((key) => setAttribute(node, namespace, key, attributes[key]));
+  Object.keys(attributes).forEach((key) => node.setAttribute(key, attributes[key]));
   Object.keys(events).forEach((event) => node.addEventListener(...[event].concat(events[event])));
 
   if (typeof content === 'string') {
